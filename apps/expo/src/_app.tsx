789f952256932033a03e57658f18b48ac7833945
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-21 23:33:28 +0200

    expo app

---
+++ apps/expo/src/_app.tsx
@@ -0,0 +1,23 @@
+import { registerRootComponent } from "expo";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+registerRootComponent(App);

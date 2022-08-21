7cfaa15399405b74638d7fe1ebfface783ccb4d3
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-22 00:32:53 +0200

    consume mock blog api

--- apps/expo/src/screens/home.tsx
+++ apps/expo/src/screens/home.tsx
@@ -1,9 +1,32 @@
-import { View, Text } from "react-native";
+import { SafeAreaView, View, Text } from "react-native";
+import { FlashList } from "@shopify/flash-list";
+import type { inferProcedureOutput } from "@trpc/server";
+import type { AppRouter } from "@acme/api";
+import { trpc } from "../utils/trpc";
+
+const PostCard: React.FC<{
+  post: inferProcedureOutput<AppRouter["post"]["all"]>[number];
+}> = ({ post }) => {
+  return (
+    <View className="p-4">
+      <Text className="text-2xl font-bold text-gray-800">{post.title}</Text>
+      <Text className="text-gray-600">{post.content}</Text>
+    </View>
+  );
+};
 
 export const HomeScreen = () => {
+  const postQuery = trpc.post.all.useQuery();
+
   return (
-    <View className="flex flex-1 bg-white items-center justify-center">
-      <Text className="font-bold text-blue-800">Home Screen</Text>
-    </View>
+    <SafeAreaView>
+      <View className="h-full w-full">
+        <FlashList
+          data={postQuery.data}
+          estimatedItemSize={20}
+          renderItem={(p) => <PostCard post={p.item} />}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { trpc } from "../utils/trpc";
+import type { inferProcedureOutput } from "@trpc/server";
+import type { AppRouter } from "@acme/api";
+
+const PostCard: React.FC<{
+  post: inferProcedureOutput<AppRouter["post"]["all"]>[number];
+}> = ({ post }) => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold text-gray-800">{post.title}</h2>
+      <p className="text-gray-600">{post.content}</p>
+    </div>
+  );
+};
 
 const Home: NextPage = () => {
-  const hello = trpc.proxy.example.hello.useQuery({ text: "from tRPC" });
+  const postQuery = trpc.proxy.post.all.useQuery();
 
   return (
     <>
@@ -16,31 +29,16 @@ const Home: NextPage = () => {
         <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
           Create <span className="text-purple-300">T3</span> App
         </h1>
-        <p className="text-2xl text-gray-700">This stack uses:</p>
-        <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
-          <TechnologyCard
-            name="NextJS"
-            description="The React framework for production"
-            documentation="https://nextjs.org/"
-          />
-          <TechnologyCard
-            name="TypeScript"
-            description="Strongly typed programming language that builds on JavaScript, giving you better tooling at any scale"
-            documentation="https://www.typescriptlang.org/"
-          />
-          <TechnologyCard
-            name="TailwindCSS"
-            description="Rapidly build modern websites without ever leaving your HTML"
-            documentation="https://tailwindcss.com/"
-          />
-          <TechnologyCard
-            name="tRPC"
-            description="End-to-end typesafe APIs made easy"
-            documentation="https://trpc.io/"
-          />
-        </div>
         <div className="flex items-center justify-center w-full pt-6 text-2xl text-blue-500">
-          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
+          {postQuery.data ? (
+            <div className="flex flex-col gap-4">
+              {postQuery.data?.map((p) => {
+                return <PostCard post={p} />;
+              })}
+            </div>
+          ) : (
+            <p>Loading..</p>
+          )}
         </div>
       </main>
     </>
@@ -48,31 +46,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-// Technology component
-interface TechnologyCardProps {
-  name: string;
-  description: string;
-  documentation: string;
-}
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a
-        className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </a>
-    </section>
-  );
-};

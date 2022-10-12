5b6e374e6312f74986b978e4d01b59d8a3148008
Nelson Rowley <47508642+nrowley@users.noreply.github.com>
2022-10-12 20:27:09 +0100

    fix: rename expo entry file (#32)
    * fix: register app from root.
    
    * Apply suggestions from code review
    
    * export app
    
    Co-authored-by: Julius Marminge <julius0216@outlook.com>

--- apps/expo/src/_app.tsx
+++ apps/expo/src/_app.tsx
@@ -1,4 +1,3 @@
-import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -6,7 +5,7 @@ import { TRPCProvider } from "./utils/trpc";
 
 import { HomeScreen } from "./screens/home";
 
-const App = () => {
+export const App = () => {
   return (
     <TRPCProvider>
       <SafeAreaProvider>
@@ -16,5 +15,3 @@ const App = () => {
     </TRPCProvider>
   );
 };
-
-registerRootComponent(App);

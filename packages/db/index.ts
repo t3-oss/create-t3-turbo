e55953e1b217854e1afedfaeab7523e84683ef4f
Braedin Jared <braedinjared@gmail.com>
2022-10-19 19:35:58 -0500

    Prevent creation of multiple prisma clients

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,5 +1,23 @@
 import { PrismaClient } from "./.generated/client";
 
-export const prisma = new PrismaClient({
-  log: ["query"],
-});
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient({
+    log: ["query"],
+  });
+  console.log("Production: Created DB connection.");
+} else {
+  // @ts-ignore
+  if (!global.prisma) {
+    // @ts-ignore
+    global.prisma = new PrismaClient({
+      log: ["query"],
+    });
+    console.log("Development: Created DB connection.");
+  }
+
+  // @ts-ignore
+  prisma = global.prisma;
+}
+
+export { prisma };

9fbed417011648c2d00cc5a56e07b931da5ee972
Ernesto Resende <dev@ernestoresende.com>
2022-10-20 18:11:54 -0700

    prevent instantiating multiple prisma clients on next

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,5 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient({
-  log: ["query"],
-});
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+
+export type { PrismaClient };

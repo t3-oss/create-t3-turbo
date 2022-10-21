58680c5864d7498db98d7a96d10ef1ed78aac074
Braedin Jared <braedinjared@gmail.com>
2022-10-21 02:52:56 -0500

    Prevent creation of multiple prisma clients (#41)

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,5 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
-  log: ["query"],
-});
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}

f30c8af893f11f0145c92b4845dd9bb14cfe4df4
Ernesto Resende <dev@ernestoresende.com>
2022-10-21 02:18:29 -0700

    Enable Prisma Client caching by Turbo and hoist Client artifacts (#46)
    * prevent instantiating multiple prisma clients on next
    
    * hoist prisma to project root
    
    * enable caching on db-generate
    
    * revert non-intentional code-style change
    
    * Update packages/db/index.ts
    
    Co-authored-by: Julius Marminge <julius0216@outlook.com>

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
+  // allow global `var` declarations
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }

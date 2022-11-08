3f7170c70d1a6d24fa8b29010f864361547df93a
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-11-08 14:28:33 +0100

    rename config

--- apps/nextjs/postcss.config.cjs
+++ apps/nextjs/postcss.config.cjs
@@ -1,1 +1,1 @@
-module.exports = require("@acme/tailwind-config/postcss.config.js")
+module.exports = require("@acme/tailwind-config/postcss");
--- packages/config/tailwind/index.js
+++ packages/config/tailwind/index.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}", "./src/_app.tsx"],
+  content: ["./src/**/*.{ts,tsx}", "./src/_app.tsx"],
   theme: {
     extend: {},
   },

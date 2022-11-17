64852bede896cbf940571251a430b4c68f1ce664
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-11-17 11:51:53 +0100

    update homepage to new ct3a style

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -2,8 +2,8 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
-
 import type { AppType } from "next/app";
+
 import { trpc } from "../utils/trpc";
 
 const MyApp: AppType<{ session: Session | null }> = ({

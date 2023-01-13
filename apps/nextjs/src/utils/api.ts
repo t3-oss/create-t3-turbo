022946e1ad0a253451217a8dc735b8314318067f
Radmacher <66096031+rdmchr@users.noreply.github.com>
2023-01-13 11:07:39 +0100

    Fixed comment (#113)
    * Fixed comment
    
    * Update apps/nextjs/src/utils/api.ts
    
    Co-authored-by: Julius Marminge <julius0216@outlook.com>

--- apps/nextjs/src/utils/api.ts
+++ apps/nextjs/src/utils/api.ts
@@ -1,4 +1,3 @@
-// src/utils/trpc.ts
 import { createTRPCNext } from "@trpc/next";
 import { httpBatchLink, loggerLink } from "@trpc/client";
 import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";

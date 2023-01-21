8b5ed7098192a6a594c37a7ab2dcaca07bceb410
Piotr <53144702+PiotrekPKP@users.noreply.github.com>
2023-01-21 23:50:38 +0100

    fix: check if lines are already appended (#122)

--- apps/expo/expo-plugins/with-modify-gradle.js
+++ apps/expo/expo-plugins/with-modify-gradle.js
@@ -10,20 +10,31 @@ const { withProjectBuildGradle } = require("@expo/config-plugins");
 
 module.exports = (config) => {
   return withProjectBuildGradle(config, (config) => {
-    config.modResults.contents = config.modResults.contents.replace(
-      "buildscript {",
-      `buildscript {
+    if (!config.modResults.contents.includes("ext.getPackageJsonVersion =")) {
+      config.modResults.contents = config.modResults.contents.replace(
+        "buildscript {",
+        `buildscript {
     ext.getPackageJsonVersion = { packageName ->
         new File(['node', '--print', "JSON.parse(require('fs').readFileSync(require.resolve('\${packageName}/package.json'), 'utf-8')).version"].execute(null, rootDir).text.trim())
     }`,
-    );
+      );
+    }
 
-    config.modResults.contents = config.modResults.contents.replace(
-      "ext {",
-      `ext {
-        reactNativeVersion = "\${ext.getPackageJsonVersion('react-native')}"
+    if (!config.modResults.contents.includes("reactNativeVersion =")) {
+      config.modResults.contents = config.modResults.contents.replace(
+        "ext {",
+        `ext {
+        reactNativeVersion = "\${ext.getPackageJsonVersion('react-native')}"`,
+      );
+    }
+
+    if (!config.modResults.contents.includes("expoPackageVersion =")) {
+      config.modResults.contents = config.modResults.contents.replace(
+        "ext {",
+        `ext {
         expoPackageVersion = "\${ext.getPackageJsonVersion('expo')}"`,
-    );
+      );
+    }
 
     return config;
   });

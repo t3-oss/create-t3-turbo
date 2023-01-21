0888db07428a96ae3b54e4a19742e637d3b65fca
PiotrekPKP <peterpkppeter@gmail.com>
2023-01-21 13:25:06 +0100

    feat: add comments

--- apps/expo/app/_layout.tsx
+++ apps/expo/app/_layout.tsx
@@ -4,6 +4,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { TRPCProvider } from "../src/utils/api";
 import { Stack } from "expo-router";
 
+// This is your app's layout. The <Stack /> component is where your app's screens will be rendered.
 const Layout: React.FC = () => {
   return (
     <TRPCProvider>
--- apps/expo/index.ts
+++ apps/expo/index.ts
@@ -1,1 +1,4 @@
+// This is the entry point for the Expo app.
+// It is responsible for setting up the app and loading the root component.
+// It is also responsible for setting up the expo-router package.
 import "expo-router/entry";

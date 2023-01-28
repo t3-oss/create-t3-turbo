304ad450edc003641493536b6eb8ec6d688986a8
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-01-28 23:32:54 +0100

    version bumps

--- packages/auth/src/get-session.ts
+++ packages/auth/src/get-session.ts
@@ -3,17 +3,16 @@ import type {
   NextApiRequest,
   NextApiResponse,
 } from "next";
-import { unstable_getServerSession } from "next-auth";
+import { getServerSession as $getServerSession } from "next-auth";
 
 import { authOptions } from "./auth-options";
 
-export const getServerSession = async (
-  ctx:
-    | {
-        req: GetServerSidePropsContext["req"];
-        res: GetServerSidePropsContext["res"];
-      }
-    | { req: NextApiRequest; res: NextApiResponse },
-) => {
-  return await unstable_getServerSession(ctx.req, ctx.res, authOptions);
+type GetServerSessionContext =
+  | {
+      req: GetServerSidePropsContext["req"];
+      res: GetServerSidePropsContext["res"];
+    }
+  | { req: NextApiRequest; res: NextApiResponse };
+export const getServerSession = (ctx: GetServerSessionContext) => {
+  return $getServerSession(ctx.req, ctx.res, authOptions);
 };

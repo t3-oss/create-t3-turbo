fb7a3846aa6c5deac9a726010a37f66f374fc247
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-01-28 22:59:06 +0100

    refactor auth a bit

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,19 +1,3 @@
 export { authOptions } from "./src/auth-options";
 export { getServerSession } from "./src/get-session";
 export type { Session } from "next-auth";
-
-import { DefaultSession } from "next-auth";
-/**
- * Module augmentation for `next-auth` types
- * Allows us to add custom properties to the `session` object
- * and keep type safety
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
-
-declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-    } & DefaultSession["user"];
-  }
-}

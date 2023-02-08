c04892235fc232c43b7b7bdbaa4c48ad0df38905
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-02-08 21:20:25 +0100

    tw config

--- apps/expo/tailwind.config.cjs
+++ apps/expo/tailwind.config.cjs
@@ -2,5 +2,5 @@
 module.exports = {
   presets: [require("@acme/tailwind-config")],
   // We have to include those files so that expo-router's pages directory is parsed by nativewind
-  content: ["./app/**/*.{ts,tsx}", "./app/*.{ts,tsx}"],
+  content: ["./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
 };

9323182f20c8670ab25bfd5f59b19d400bf2a846
PiotrekPKP <peterpkppeter@gmail.com>
2023-02-08 22:04:08 +0100

    fix: prettify

--- apps/expo/app/post/[id].tsx
+++ apps/expo/app/post/[id].tsx
@@ -4,23 +4,27 @@ import { SplashScreen, Stack, useSearchParams } from "expo-router";
 import { api } from "../../src/utils/api";
 
 const Post: React.FC = () => {
-  const {id} = useSearchParams();
+  const { id } = useSearchParams();
   const { data } = api.post.byId.useQuery(id || "", { enabled: !!id });
 
   if (!data) return <SplashScreen />;
 
   return (
     <SafeAreaView className="bg-[#1F104A]">
-      <Stack.Screen options={{ title: !data.title ? "Untitled" : data.title}} />
+      <Stack.Screen
+        options={{ title: !data.title ? "Untitled" : data.title }}
+      />
       <View className="h-full w-full p-4">
         <Text
-            className={`py-2 text-3xl font-bold text-white ${!data.content ? "italic" : ""}`}
-          >
-            {data.title || "Untitled"}
-          </Text>
-          <Text className={`py-4 text-white ${!data.content ? "italic" : ""}`}>
-            {data.content || "No content"}
-          </Text>
+          className={`py-2 text-3xl font-bold text-white ${
+            !data.content ? "italic" : ""
+          }`}
+        >
+          {data.title || "Untitled"}
+        </Text>
+        <Text className={`py-4 text-white ${!data.content ? "italic" : ""}`}>
+          {data.content || "No content"}
+        </Text>
       </View>
     </SafeAreaView>
   );

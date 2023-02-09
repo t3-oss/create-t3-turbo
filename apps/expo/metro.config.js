4e2c069ded79eabb5a4bce99d065752ee1627563
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-02-10 00:10:18 +0100

    disable an option

--- apps/expo/metro.config.js
+++ apps/expo/metro.config.js
@@ -19,6 +19,6 @@ config.resolver.nodeModulesPaths = [
   path.resolve(workspaceRoot, "node_modules"),
 ];
 // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-config.resolver.disableHierarchicalLookup = true;
+// config.resolver.disableHierarchicalLookup = true;
 
 module.exports = config;

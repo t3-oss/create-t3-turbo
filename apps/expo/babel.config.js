84cdb720112662c7c0ebeffade8762602e7ba86d
Piotr <53144702+PiotrekPKP@users.noreply.github.com>
2023-03-12 23:25:01 +0100

    refactor: move expo router into src (no ts error) (#200)

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,5 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
+
+  // Make Expo Router run from `src/app` instead of `app`.
+  // Path is relative to `/node_modules/expo-router`
+  process.env.EXPO_ROUTER_APP_ROOT = "../../apps/expo/src/app";
+
   return {
     plugins: ["nativewind/babel", require.resolve("expo-router/babel")],
     presets: ["babel-preset-expo"],

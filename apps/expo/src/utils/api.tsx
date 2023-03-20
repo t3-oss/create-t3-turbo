656965aff7db271e5e080242c4a3ce4dad5d25f8
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-03-20 11:06:50 +0100

    chore: add note about production url

--- apps/expo/src/utils/api.tsx
+++ apps/expo/src/utils/api.tsx
@@ -22,6 +22,9 @@ const getBaseUrl = () => {
    * Gets the IP address of your host-machine. If it cannot automatically find it,
    * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
    * you don't have anything else running on it, or you'd have to change it.
+   *
+   * **NOTE**: This is only for development. In production, you'll want to set the
+   * baseUrl to your production API URL.
    */
   const localhost = Constants.manifest?.debuggerHost?.split(":")[0];
   if (!localhost) {

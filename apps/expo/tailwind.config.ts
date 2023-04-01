813341f085f78be606280b36b65aae0edb3e4009
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-04-01 12:04:13 +0200

    skip ts config in expo

--- apps/expo/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import { type Config } from "tailwindcss";
-
-import baseConfig from "@acme/tailwind-config";
-
-export default {
-  presets: [baseConfig],
-  content: ["./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-} satisfies Config;

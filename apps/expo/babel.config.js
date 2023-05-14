501e567b0c39b9140a59a37ddb81ef8aed768785
cjkihl <carljohan.kihl@gmail.com>
2023-05-15 00:41:29 +0800

    Transpile tailwind.config.ts in Expo

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,3 +1,23 @@
+const jiti = require("jiti");
+const { transform } = require("sucrase");
+
+
+let _tailwindConfig = null;
+/**
+ * Transpiles tailwind.config.ts for babel
+ * Fix until nativewind babel plugin supports tailwind.config.ts files
+ */
+function lazyTranspileTailwindConfig() {
+  return (_tailwindConfig ?? jiti(__filename, {
+    interopDefault: true,
+    transform: (options) => {
+      return transform(options.source, {
+        transforms: ["typescript", "imports"],
+      });
+    },
+  })('./tailwind.config.ts'));
+}
+
 /** @type {import("@babel/core").ConfigFunction} */
 module.exports = function (api) {
   api.cache.forever();
@@ -8,10 +28,13 @@ module.exports = function (api) {
 
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      "nativewind/babel",
-      "expo-router/babel",
+    plugins:
+      [["nativewind/babel", {
+        tailwindConfig: lazyTranspileTailwindConfig()
+      }],
+        "expo-router/babel",
       ["module-resolver", { alias: { "~": "./src" } }],
-    ],
+      ]
   };
 };
+

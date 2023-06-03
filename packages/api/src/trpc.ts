1c1613528ffdd209d5c368c6bc6081f0d6c85b51
Nicolas Keil <26151586+nkeil@users.noreply.github.com>
2023-06-03 10:44:18 -0700

    Upgrade prettier-plugin-sort-imports (#387)

--- packages/api/src/trpc.ts
+++ packages/api/src/trpc.ts
@@ -6,7 +6,7 @@
  * tl;dr - this is where all the tRPC server stuff is created and plugged in.
  * The pieces you will need to use are documented accordingly near the end
  */
-import { TRPCError, initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import superjson from "superjson";
 import { ZodError } from "zod";

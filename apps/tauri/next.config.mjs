b7698e94b2863d654e21143c63d53129e2061943
Daniel <70210356+Itsnotaka@users.noreply.github.com>
2023-06-28 18:40:47 +0800

    [ci] added tauri as desktop app

---
+++ apps/tauri/next.config.mjs
@@ -0,0 +1,15 @@
+// Importing env files here to validate on build
+import "./src/env.mjs";
+import "@acme/auth/env.mjs";
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: ["@acme/api", "@acme/auth", "@acme/db"],
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+
+export default config;

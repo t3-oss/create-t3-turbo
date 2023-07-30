3583aa9b0e270af5aca8d1ae998191cbe80b1ffc
juliusmarminge <julius0216@outlook.com>
2023-07-30 16:24:58 +0200

    handle error

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const postRouter = createTRPCRouter({
   all: publicProcedure.query(({ ctx }) => {
@@ -11,7 +11,7 @@ export const postRouter = createTRPCRouter({
     .query(({ ctx, input }) => {
       return ctx.prisma.post.findFirst({ where: { id: input.id } });
     }),
-  create: publicProcedure
+  create: protectedProcedure
     .input(
       z.object({
         title: z.string().min(1),

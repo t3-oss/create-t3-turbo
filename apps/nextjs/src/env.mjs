7556e38caf4cc8bed4e8d048bdaccab37a77f7c9
juliusmarminge <julius0216@outlook.com>
2023-07-30 16:18:32 +0200

    patch

--- apps/nextjs/src/env.mjs
+++ apps/nextjs/src/env.mjs
@@ -2,6 +2,13 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
+  shared: {
+    VERCEL_URL: z
+      .string()
+      .optional()
+      .transform((v) => (v ? `https://${v}` : undefined)),
+    PORT: z.coerce.number().default(3000),
+  },
   /**
    * Specify your server-side environment variables schema here. This way you can ensure the app isn't
    * built with invalid env vars.
@@ -20,6 +27,8 @@ export const env = createEnv({
    * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
    */
   runtimeEnv: {
+    VERCEL_URL: process.env.VERCEL_URL,
+    PORT: process.env.PORT,
     DATABASE_URL: process.env.DATABASE_URL,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },

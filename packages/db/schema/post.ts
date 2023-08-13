3bfbb4e7c504aec0af450abd6d11e64e40f34b9a
juliusmarminge <julius0216@outlook.com>
2023-08-13 19:50:46 +0200

    fix post name

--- packages/db/schema/post.ts
+++ packages/db/schema/post.ts
@@ -1,15 +1,8 @@
-import { sql } from "drizzle-orm";
-import {
-  mysqlTableCreator,
-  serial,
-  timestamp,
-  uniqueIndex,
-  varchar,
-} from "drizzle-orm/mysql-core";
+import { serial, timestamp, varchar } from "drizzle-orm/mysql-core";
 
 import { mySqlTable } from "./_table";
 
-export const post = mySqlTable("example", {
+export const post = mySqlTable("post", {
   id: serial("id").primaryKey(),
   title: varchar("name", { length: 256 }).notNull(),
   content: varchar("content", { length: 256 }).notNull(),

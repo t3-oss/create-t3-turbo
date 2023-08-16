c6391f38a44b5c198afe2678a51e86db70b47189
juliusmarminge <julius0216@outlook.com>
2023-08-16 19:30:20 +0200

    port some changes from monorepo-masters

---
+++ tooling/prettier/index.mjs
@@ -0,0 +1,29 @@
+/** @typedef  {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+const config = {
+    plugins: [
+      "@ianvs/prettier-plugin-sort-imports",
+      "prettier-plugin-tailwindcss",
+    ],
+    tailwindConfig: "../../tooling/tailwind",
+    importOrder: [
+      "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+      "^(next/(.*)$)|^(next$)",
+      "^(expo(.*)$)|^(expo$)",
+      "<THIRD_PARTY_MODULES>",
+      "",
+      "^@acme/(.*)$",
+      "",
+      "^~/", 
+      "^[../]", 
+      "^[./]"
+    ],
+    importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+    importOrderTypeScriptVersion: "4.4.0",
+  };
+  
+  export default config;
+  

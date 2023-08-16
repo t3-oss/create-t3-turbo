cc928da2aa05583dd0c4fd47c9faf3e984614ba6
juliusmarminge <julius0216@outlook.com>
2023-08-16 19:42:10 +0200

    tooling pkgs

--- tooling/prettier/index.mjs
+++ tooling/prettier/index.mjs
@@ -4,26 +4,25 @@
 
 /** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
 const config = {
-    plugins: [
-      "@ianvs/prettier-plugin-sort-imports",
-      "prettier-plugin-tailwindcss",
-    ],
-    tailwindConfig: "../../tooling/tailwind",
-    importOrder: [
-      "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
-      "^(next/(.*)$)|^(next$)",
-      "^(expo(.*)$)|^(expo$)",
-      "<THIRD_PARTY_MODULES>",
-      "",
-      "^@acme/(.*)$",
-      "",
-      "^~/", 
-      "^[../]", 
-      "^[./]"
-    ],
-    importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-    importOrderTypeScriptVersion: "4.4.0",
-  };
-  
-  export default config;
-  
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  tailwindConfig: "../../tooling/tailwind",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+    "^(next/(.*)$)|^(next$)",
+    "^(expo(.*)$)|^(expo$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@acme/(.*)$",
+    "",
+    "^~/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "4.4.0",
+};
+
+export default config;

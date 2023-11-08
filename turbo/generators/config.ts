bf7f533d22a1e2cc1db8ae9701157a8e7afd9372
juliusmarminge <julius0216@outlook.com>
2023-11-08 23:39:05 +0100

    fix

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -72,9 +72,10 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
          * Install deps and format everything
          */
         if ("name" in answers && typeof answers.name === "string") {
-          execSync("pnpm dlx sherif@latest --fix", {
-            stdio: "inherit",
-          });
+          // execSync("pnpm dlx sherif@latest --fix", {
+          //   stdio: "inherit",
+          // });
+          execSync("pnpm i", { stdio: "inherit" });
           execSync(
             `pnpm prettier --write packages/${answers.name}/** --list-different`,
           );

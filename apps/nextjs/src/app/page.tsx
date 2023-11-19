ac935be7890aac062b9ced14a55f4f32045005f4
juliusmarminge <julius0216@outlook.com>
2023-11-19 17:07:20 +0100

    ignore lint

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -11,7 +11,11 @@ import {
 export const runtime = "edge";
 
 export default async function HomePage() {
+  // You don't need to fetch these here, just showing different usages
+  // If you don't want the Suspense loading state, you could pass these
+  // posts as props as use as initialData in the query.
   const posts = await api.post.all.query();
+  console.log("RSC Posts:", posts);
 
   return (
     <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">

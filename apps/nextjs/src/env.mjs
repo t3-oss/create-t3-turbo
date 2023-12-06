8fa2960ca3026446cfc1c63f7a909a6af72ea240
t4t5u0 <tatsuo.yamamoto@caname.co.jp>
2023-12-06 12:07:36 +0900

    reference process.env

--- apps/nextjs/src/env.mjs
+++ apps/nextjs/src/env.mjs
@@ -13,6 +13,7 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app isn't
    * built with invalid env vars.
    */
+  environment
   server: {
     DB_USERNAME: z.string(),
     DB_PASSWORD: z.string(),
@@ -32,10 +33,10 @@ export const env = createEnv({
   runtimeEnv: {
     VERCEL_URL: process.env.VERCEL_URL,
     PORT: process.env.PORT,
-    DB_USERNAME: z.string(),
-    DB_PASSWORD: z.string(),
-    DB_HOST: z.string(),
-    DB_NAME: z.string(),
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_HOST: process.env.DB_HOST,
+    DB_NAME: process.env.DB_NAME,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   skipValidation:

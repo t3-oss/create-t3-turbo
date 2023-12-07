c0968cc8731c870cec628b9fafe41cb2fc410615
Hichem Fantar <hichemfantar2049@gmail.com>
2023-12-07 20:01:09 +0100

    implement react native compatible logger (#781)

--- apps/expo/src/utils/api.tsx
+++ apps/expo/src/utils/api.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Constants from "expo-constants";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
+import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import superjson from "superjson";
 
@@ -57,6 +57,12 @@ export function TRPCProvider(props: { children: React.ReactNode }) {
             headers.set("x-trpc-source", "expo-react");
             return Object.fromEntries(headers);
           },
+        }),
+        loggerLink({
+          enabled: (opts) =>
+            process.env.NODE_ENV === "development" ||
+            (opts.direction === "down" && opts.result instanceof Error),
+          colorMode: "ansi",
         }),
       ],
     }),

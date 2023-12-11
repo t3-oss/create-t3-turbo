d523ae47f8110b30a46d08d8d42ec811f6ab2151
Hichem Fantar <hichemfantar2049@gmail.com>
2023-12-11 14:36:27 +0100

    Limit fetched posts (#793)

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -7,7 +7,10 @@ import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 export const postRouter = createTRPCRouter({
   all: publicProcedure.query(({ ctx }) => {
     // return ctx.db.select().from(schema.post).orderBy(desc(schema.post.id));
-    return ctx.db.query.post.findMany({ orderBy: desc(schema.post.id) });
+    return ctx.db.query.post.findMany({
+      orderBy: desc(schema.post.id),
+      limit: 10,
+    });
   }),
 
   byId: publicProcedure

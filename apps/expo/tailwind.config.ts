677ea2fd97367a4695c4c224fbaab48bb4425bbd
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2023-12-14 16:32:21 +0100

    fix typo

--- apps/expo/tailwind.config.ts
+++ apps/expo/tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-// @ts-expect-error no types
+// @ts-expect-error - no types
 import nativewind from "nativewind/preset";
 
 import baseConfig from "@acme/tailwind-config";

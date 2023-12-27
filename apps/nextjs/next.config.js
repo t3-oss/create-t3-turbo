50608c2bde822c38ed3937d9cb70abb2ec53bc8e
juliusmarminge <julius0216@outlook.com>
2023-12-27 23:49:25 +0100

    static-test

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -7,6 +7,9 @@ const config = {
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
   transpilePackages: ["@acme/api", "@acme/auth", "@acme/db"],
+  // experimental: {
+  //   ppr: true,
+  // },
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
---
+++ apps/nextjs/src/app/static-test/page.tsx
@@ -0,0 +1,3 @@
+export default function StaticPage() {
+  return <div>I am fully static</div>;
+}

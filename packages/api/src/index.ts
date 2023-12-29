b53e6f1f337524e7df08e58dbba6df67ffc5b628
juliusmarminge <julius0216@outlook.com>
2023-12-29 14:03:51 +0100

    update-trpc

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,18 +1,33 @@
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 
 import type { AppRouter } from "./root";
+import { appRouter } from "./root";
+import { createCallerFactory, createTRPCContext } from "./trpc";
 
-export { appRouter, type AppRouter } from "./root";
-export { createTRPCContext } from "./trpc";
+/**
+ * Create a server-side caller for the tRPC API
+ * @example
+ * const trpc = createCaller(createContext);
+ * const res = await trpc.post.all();
+ *       ^? Post[]
+ */
+const createCaller = createCallerFactory(appRouter);
 
 /**
  * Inference helpers for input types
- * @example type HelloInput = RouterInputs['example']['hello']
+ * @example
+ * type PostByIdInput = RouterInputs['post']['byId']
+ *      ^? { id: number }
  **/
-export type RouterInputs = inferRouterInputs<AppRouter>;
+type RouterInputs = inferRouterInputs<AppRouter>;
 
 /**
  * Inference helpers for output types
- * @example type HelloOutput = RouterOutputs['example']['hello']
+ * @example
+ * type AllPostsOutput = RouterOutputs['post']['all']
+ *      ^? Post[]
  **/
-export type RouterOutputs = inferRouterOutputs<AppRouter>;
+type RouterOutputs = inferRouterOutputs<AppRouter>;
+
+export { createTRPCContext, appRouter, createCaller };
+export type { AppRouter, RouterInputs, RouterOutputs };

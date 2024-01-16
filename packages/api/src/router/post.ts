cea783769692a5d5911a372209aef74dedccf4a3
Julius Marminge <julius0216@outlook.com>
2024-01-16 14:59:02 +0100

    Update post.ts

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -6,7 +6,7 @@ import { CreatePostSchema } from "@acme/validators";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const postRouter = createTRPCRouter({
-  all: protectedProcedure.query(({ ctx }) => {
+  all: publicProcedure.query(({ ctx }) => {
     // return ctx.db.select().from(schema.post).orderBy(desc(schema.post.id));
     return ctx.db.query.post.findMany({
       orderBy: desc(schema.post.id),

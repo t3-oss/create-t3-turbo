6cb349078618dc733fbb9bbdd2e6e3f50cc6382e
juliusmarminge <julius0216@outlook.com>
2024-01-28 11:15:51 +0100

    split tw

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
-import baseConfig from "@acme/tailwind-config";
+import baseConfig from "@acme/tailwind-config/web";
 
 export default {
   // We need to append the path to the UI package to the content array so that

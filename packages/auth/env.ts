ae2d8e3945dc643380312108dbacb88747081c33
juliusmarminge <julius0216@outlook.com>
2024-02-08 19:58:59 +0100

    remove need for AUTH_URL

--- packages/auth/env.ts
+++ packages/auth/env.ts
@@ -9,13 +9,6 @@ export const env = createEnv({
       process.env.NODE_ENV === "production"
         ? z.string().min(1)
         : z.string().min(1).optional(),
-    AUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url(),
-    ),
   },
   client: {},
   experimental__runtimeEnv: {},

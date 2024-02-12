28b3525f6acdc07c36c03c818aa8832b0de35802
Cuhadari-Deniz <36639958+Cuhadari-Deniz@users.noreply.github.com>
2024-02-12 22:36:37 +0100

    fixed jiti import for windows (#886)
    * fixed jiti import for windows
    
    * fixed using fileURLToPath
    
    * fixed typo
    
    * made it a oneliner
    
    * fixed formatting

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -1,6 +1,7 @@
+import { fileURLToPath } from "url";
 import _jiti from "jiti";
 
-const jiti = _jiti(new URL(import.meta.url).pathname);
+const jiti = _jiti(fileURLToPath(import.meta.url));
 
 // Import env files to validate at build time. Use jiti so we can load .ts files in here.
 jiti("./src/env");

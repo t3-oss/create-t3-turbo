67ea470298af32c0deecc329a6df80bb6817ccc4
juliusmarminge <julius0216@outlook.com>
2024-02-22 17:01:53 +0100

    chore: update drizzle

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,4 +1,4 @@
-import { connect } from "@planetscale/database";
+import { Client } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 
 import * as auth from "./schema/auth";
@@ -10,10 +10,10 @@ export { mySqlTable as tableCreator } from "./schema/_table";
 
 export * from "drizzle-orm";
 
-const connection = connect({
+const psClient = new Client({
   host: process.env.DB_HOST!,
   username: process.env.DB_USERNAME!,
   password: process.env.DB_PASSWORD!,
 });
 
-export const db = drizzle(connection, { schema });
+export const db = drizzle(psClient, { schema });

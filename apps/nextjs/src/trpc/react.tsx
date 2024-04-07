39eea4c6c899dae73aae04cc29f5231fedb4b9ad
hoangtrung <trungnh@solashi.com>
2024-04-08 01:05:36 +0700

    chore: update default staleTime for query client

--- apps/nextjs/src/trpc/react.tsx
+++ apps/nextjs/src/trpc/react.tsx
@@ -14,7 +14,7 @@ const createQueryClient = () =>
       queries: {
         // With SSR, we usually want to set some default staleTime
         // above 0 to avoid refetching immediately on the client
-        staleTime: 60 * 1000,
+        staleTime: 30 * 1000,
       },
     },
   });

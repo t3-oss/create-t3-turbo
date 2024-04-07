f0ee3becd0429af4e70a01fafcf10d65c4e81eac
juliusmarminge <julius0216@outlook.com>
2024-04-08 01:20:13 +0200

    preparre ignore pattern in all configs

--- packages/auth/eslint.config.js
+++ packages/auth/eslint.config.js
@@ -1,4 +1,9 @@
 import baseConfig from "@acme/eslint-config/base";
 
 /** @type {import('typescript-eslint').Config} */
-export default [...baseConfig];
+export default [
+  {
+    ignores: [],
+  },
+  ...baseConfig,
+];

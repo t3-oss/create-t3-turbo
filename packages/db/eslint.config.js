9866e2e96b250326b7c7d420c9cf56f37fe46c3c
juliusmarminge <julius0216@outlook.com>
2024-04-08 01:06:14 +0200

    reference types

--- packages/db/eslint.config.js
+++ packages/db/eslint.config.js
@@ -1,3 +1,6 @@
+// FIXME: This kinda stinks...
+/// <reference types="../../tooling/eslint/types.d.ts" />
+
 import baseConfig from "@acme/eslint-config/base";
 
 /** @type {import('typescript-eslint').Config} */
--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,3 +1,6 @@
+// FIXME: This kinda stinks...
+/// <reference types="../../tooling/eslint/types.d.ts" />
+
 import baseConfig from "@acme/eslint-config/base";
 import reactConfig from "@acme/eslint-config/react";
 

8ed5ccfac858ef2fb7fa3cdc33bc66f6af577cbb
juliusmarminge <julius0216@outlook.com>
2024-04-25 18:53:52 +0200

    update auth and drizzle adapter

--- packages/auth/src/config.ts
+++ packages/auth/src/config.ts
@@ -2,7 +2,7 @@ import type { DefaultSession, NextAuthConfig } from "next-auth";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import Discord from "next-auth/providers/discord";
 
-import { db, tableCreator } from "@acme/db";
+import { db, schema } from "@acme/db";
 
 declare module "next-auth" {
   interface Session {
@@ -13,7 +13,12 @@ declare module "next-auth" {
 }
 
 export const authConfig = {
-  adapter: DrizzleAdapter(db, tableCreator),
+  adapter: DrizzleAdapter(db, {
+    usersTable: schema.users,
+    accountsTable: schema.accounts,
+    sessionsTable: schema.sessions,
+    verificationTokensTable: schema.verificationTokens,
+  }),
   providers: [Discord],
   callbacks: {
     session: (opts) => {

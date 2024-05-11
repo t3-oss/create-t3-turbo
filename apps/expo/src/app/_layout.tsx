03a2f64e84e23736ca71f8c9fd08cf5564adadef
juliusmarminge <julius0216@outlook.com>
2024-05-11 23:29:47 +0200

    add textdecoder polyfill

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -1,11 +1,12 @@
+import "@bacons/text-decoder/install";
+
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "nativewind";
 
 import { TRPCProvider } from "~/utils/api";
 
 import "../styles.css";
-
-import { useColorScheme } from "nativewind";
 
 // This is the main layout of the app
 // It wraps your pages with the providers they need

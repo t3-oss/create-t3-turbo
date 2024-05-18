3442dd21f0ace022e1b38d6c9ace707aeedaa6db
Gabriel Bianchi <gdbianchii@gmail.com>
2024-05-18 19:34:33 -0300

    Use .find() instead of always first cookie (#1043)

--- apps/nextjs/src/app/api/auth/[...nextauth]/route.ts
+++ apps/nextjs/src/app/api/auth/[...nextauth]/route.ts
@@ -32,8 +32,11 @@ export const GET = async (
     cookies().delete(EXPO_COOKIE_NAME);
 
     const authResponse = await DEFAULT_GET(req);
-    const setCookie = authResponse.headers.getSetCookie()[0];
+    const setCookie = authResponse.headers
+      .getSetCookie()
+      .find((cookie) => cookie.startsWith("authjs.session-token"));
     const match = setCookie?.match(AUTH_COOKIE_PATTERN)?.[1];
+      
     if (!match)
       throw new Error(
         "Unable to find session cookie: " +

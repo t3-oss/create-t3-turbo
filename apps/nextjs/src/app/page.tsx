744d2b648e871f9cbd9e1a6583ba46998e289a94
Aidan Kinzett <aidan@labrys.io>
2024-05-18 14:15:14 +1000

    feat: add mongoDB text to homepage

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -8,8 +8,6 @@ import {
   PostList,
 } from "./_components/posts";
 
-export const runtime = "edge";
-
 export default function HomePage() {
   // You can await this here if you don't want to show Suspense fallback below
   const posts = api.post.all();
@@ -18,7 +16,8 @@ export default function HomePage() {
     <main className="container h-screen py-16">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-primary">T3</span> Turbo
+          Create <span className="text-primary">T3</span> Turbo{" "}
+          <span className="text-green-500">MongoDB</span>
         </h1>
         <AuthShowcase />
 

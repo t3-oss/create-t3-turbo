4282647610682058481463f5433c925d9c1b9b62
Oscar Chic Farré <oscarchicfarre@gmail.com>
2024-05-28 11:23:43 +0200

    refactor: split env var inference so they can be reused

--- packages/auth/scripts/dev-env.js
+++ packages/auth/scripts/dev-env.js
@@ -5,24 +5,41 @@ import path from "path";
 import { makeGetDevEnv } from "@acme/scripts/dev-env.js";
 import { getUnusedPort, sanititisePort } from "@acme/scripts/http.js";
 
-export const getDevEnv = makeGetDevEnv([["AUTH_URL", getAuthUrlFallback]], {
-  source: `/packages/auth/scripts/dev-env.js`,
-  readme: path.relative(
-    process.cwd(),
-    path.resolve(import.meta.dirname, "../README.md"),
-  ),
+export const EnvVars = Object.freeze({
+  HOSTNAME: "HOSTNAME",
+  PORT: "PORT",
+  AUTH_URL: "AUTH_URL",
 });
 
-const NEXTJS_INITIAL_PORT =
+export const getDevEnv = makeGetDevEnv(
+  [
+    [EnvVars.HOSTNAME, hostnameFallbackFn],
+    [EnvVars.PORT, portFallbackFn],
+    [EnvVars.AUTH_URL, authUrlFallbackFn],
+  ],
+  {
+    source: `/packages/auth/scripts/dev-env.js`,
+    readme: path.relative(
+      process.cwd(),
+      path.resolve(import.meta.dirname, "../README.md"),
+    ),
+  },
+);
+
+export const NEXTJS_INITIAL_PORT =
   // eslint-disable-next-line no-restricted-properties
   sanititisePort(process.env.PORT) ??
   // TODO: read port from command?
   null ??
   3000;
 
-async function getAuthUrlFallback() {
-  const protocol = "http";
-  const port = await getUnusedPort(NEXTJS_INITIAL_PORT);
+// this is not going to work in Expo, but using it anyway as default
+const NEXTJS_INITIAL_HOSTNAME = "localhost";
+
+const NEXTJS_INITIAL_PROTOCOL = "http";
+
+/** @type {import('@acme/scripts/dev-env.js').FallbackFn} */
+export function hostnameFallbackFn() {
   const interfaces = networkInterfaces();
   /** @type {string[]} */
   const addresses = [];
@@ -34,17 +51,26 @@ async function getAuthUrlFallback() {
       }
     }
   }
-  const [address] = addresses.sort(
+  const [address = NEXTJS_INITIAL_HOSTNAME] = addresses.sort(
     (a, b) => getIPv4Priority(a) - getIPv4Priority(b),
   );
-  return address
-    ? `${protocol}://${address}:${port}`
-    : // this will not work in expo app, but trying anyway...
-      `${protocol}://localhost:${port}`;
+  return address;
+}
+
+/** @type {import('@acme/scripts/dev-env.js').FallbackFn} */
+export async function portFallbackFn() {
+  return getUnusedPort(NEXTJS_INITIAL_PORT).then(String);
+}
+
+/** @type {import('@acme/scripts/dev-env.js').FallbackFn} */
+export function authUrlFallbackFn() {
+  const protocol = NEXTJS_INITIAL_PROTOCOL;
+
+  return `${protocol}://$${EnvVars.HOSTNAME}:$${EnvVars.PORT}`;
 }
 
 /** @param {string} ipv4 */
-function getIPv4Priority(ipv4) {
+export function getIPv4Priority(ipv4) {
   switch (true) {
     case ipv4.startsWith("127."):
       return 1000;

68f2967d8d047ad562de2cdce89f9262e583ad02
juliusmarminge <julius0216@outlook.com>
2024-06-09 20:10:32 +0200

    revert

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -18,7 +18,7 @@ export default function HomePage() {
     <main className="container h-screen py-16">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-primary">T3</span> Turbooo
+          Create <span className="text-primary">T3</span> Turbo
         </h1>
         <AuthShowcase />
 

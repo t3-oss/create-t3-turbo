d1675c64f6b5568cd55a5ed3422d972ea5ae17eb
juliusmarminge <julius0216@outlook.com>
2024-06-09 19:55:38 +0200

    bump auth

--- apps/auth-proxy/routes/r/[...auth].ts
+++ apps/auth-proxy/routes/r/[...auth].ts
@@ -4,6 +4,7 @@ import { eventHandler, toWebRequest } from "h3";
 
 export default eventHandler(async (event) =>
   Auth(toWebRequest(event), {
+    basePath: "/r",
     secret: process.env.AUTH_SECRET,
     trustHost: !!process.env.VERCEL,
     redirectProxyUrl: process.env.AUTH_REDIRECT_PROXY_URL,

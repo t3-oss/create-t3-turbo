74c3c586ba21f759eb565314f70507603c6d6b3e
Julius Marminge <julius0216@outlook.com>
2024-06-09 17:23:26 +0200

    chore: bump turbo (#1070)
    * bump turbo
    
    * fmt

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -2,6 +2,7 @@
 
 import eslint from "@eslint/js";
 import importPlugin from "eslint-plugin-import";
+import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 
 /**
@@ -40,6 +41,7 @@ export default tseslint.config(
     files: ["**/*.js", "**/*.ts", "**/*.tsx"],
     plugins: {
       import: importPlugin,
+      turbo: turboPlugin,
     },
     extends: [
       eslint.configs.recommended,
@@ -48,6 +50,7 @@ export default tseslint.config(
       ...tseslint.configs.stylisticTypeChecked,
     ],
     rules: {
+      ...turboPlugin.configs.recommended.rules,
       "@typescript-eslint/no-unused-vars": [
         "error",
         { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },

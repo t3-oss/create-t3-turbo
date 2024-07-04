c6c4c4b97020dbdbff038a7ca19b73769550a2a3
juliusmarminge <julius0216@outlook.com>
2024-07-04 23:46:39 +0200

    rsc prefetch instead of passing initialData as props

--- apps/nextjs/src/trpc/react.tsx
+++ apps/nextjs/src/trpc/react.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import type { QueryClient } from "@tanstack/react-query";
 import { useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import SuperJSON from "superjson";
@@ -9,17 +10,7 @@ import SuperJSON from "superjson";
 import type { AppRouter } from "@acme/api";
 
 import { env } from "~/env";
-
-const createQueryClient = () =>
-  new QueryClient({
-    defaultOptions: {
-      queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 30 * 1000,
-      },
-    },
-  });
+import { createQueryClient } from "./query-client";
 
 let clientQueryClientSingleton: QueryClient | undefined = undefined;
 const getQueryClient = () => {
--- apps/nextjs/src/trpc/server.ts
+++ apps/nextjs/src/trpc/server.ts
@@ -1,8 +1,12 @@
 import { cache } from "react";
 import { headers } from "next/headers";
+import { createHydrationHelpers } from "@trpc/react-query/rsc";
 
+import type { AppRouter } from "@acme/api";
 import { createCaller, createTRPCContext } from "@acme/api";
 import { auth } from "@acme/auth";
+
+import { createQueryClient } from "./query-client";
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
@@ -18,4 +22,10 @@ const createContext = cache(async () => {
   });
 });
 
-export const api = createCaller(createContext);
+const getQueryClient = cache(createQueryClient);
+const caller = createCaller(createContext);
+
+export const { trpc: api, HydrateClient } = createHydrationHelpers<AppRouter>(
+  caller,
+  getQueryClient,
+);

ff5e8b58817c077231d9ac7da7cdc775436a5ab3
Jan Beck <jancbeck@users.noreply.github.com>
2024-07-21 02:13:30 +0200

    disable middleware

--- apps/nextjs/src/_middleware.ts
+++ apps/nextjs/src/_middleware.ts
@@ -6,6 +6,6 @@
 // })
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-};
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+// };

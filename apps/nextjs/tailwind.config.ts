0d5bd08c7d8859779bd8e699d06d953a8892efae
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2024-08-11 15:18:49 +0330

    tailwind: don't match node_modules in ui package

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -6,7 +6,7 @@ import baseConfig from "@acme/tailwind-config/web";
 export default {
   // We need to append the path to the UI package to the content array so that
   // those classes are included correctly.
-  content: [...baseConfig.content, "../../packages/ui/**/*.{ts,tsx}"],
+  content: [...baseConfig.content, "../../packages/ui/src/**/*.{ts,tsx}"],
   presets: [baseConfig],
   theme: {
     extend: {

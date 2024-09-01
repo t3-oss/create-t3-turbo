02390a2b46b888c8b5fd9b628ce10b68a01fbde4
Jan Beck <jancbeck@users.noreply.github.com>
2024-09-01 12:42:33 +0200

    remove payload template traces

--- apps/expo/api/index.ts
+++
@@ -1,6 +0,0 @@
-const { createRequestHandler } = require("@expo/server/adapter/vercel");
-
-module.exports = createRequestHandler({
-  build: require("path").join(__dirname, "../dist/server"),
-  mode: process.env.NODE_ENV,
-});

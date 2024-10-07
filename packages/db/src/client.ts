99f1b4e79467924a479d9f8781b5c1d7c5ea0e77
Julius Marminge <julius0216@outlook.com>
2024-10-07 19:40:37 +0200

    bump drizzle (#1206)

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -3,4 +3,4 @@ import { drizzle } from "drizzle-orm/vercel-postgres";
 
 import * as schema from "./schema";
 
-export const db = drizzle(sql, { schema });
+export const db = drizzle(sql, { schema, casing: "snake_case" });

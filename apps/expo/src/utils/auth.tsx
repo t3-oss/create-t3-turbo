e5620ddc86aa705b96dc9630d889882d152c1764
Gabriel Bianchi <gdbianchii@gmail.com>
2024-10-16 18:47:15 -0300

    Expo > Don't `router.replace()` on dismiss web view (#1079)
    Co-authored-by: dbianchii <dbianchii@gmail.com>
    Co-authored-by: juliusmarminge <julius0216@outlook.com>

--- apps/expo/src/utils/auth.tsx
+++ apps/expo/src/utils/auth.tsx
@@ -14,12 +14,14 @@ export const signIn = async () => {
     redirectTo,
   );
 
-  if (result.type !== "success") return;
+  if (result.type !== "success") return false;
   const url = Linking.parse(result.url);
   const sessionToken = String(url.queryParams?.session_token);
-  if (!sessionToken) return;
+  if (!sessionToken) throw new Error("No session token found");
 
   setToken(sessionToken);
+
+  return true;
 };
 
 export const useUser = () => {
@@ -32,7 +34,9 @@ export const useSignIn = () => {
   const router = useRouter();
 
   return async () => {
-    await signIn();
+    const success = await signIn();
+    if (!success) return;
+
     await utils.invalidate();
     router.replace("/");
   };

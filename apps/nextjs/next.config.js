536a54b0c6da5e0edbdb6e7ee9a586ba2b92e7b3
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2024-10-30 16:09:15 +0330

    revert jiti to v1

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -1,8 +1,8 @@
 import { fileURLToPath } from "url";
-import { createJiti } from "jiti";
+import createJiti from "jiti";
 
 // Import env files to validate at build time. Use jiti so we can load .ts files in here.
-await createJiti(fileURLToPath(import.meta.url)).import("./src/env");
+createJiti(fileURLToPath(import.meta.url))("./src/env");
 
 /** @type {import("next").NextConfig} */
 const config = {

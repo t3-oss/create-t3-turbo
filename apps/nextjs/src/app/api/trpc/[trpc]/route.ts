d6c3953f24f63af842d7c34b9d14ca92340bf43e
nickreynolds <nicholas.s.reynolds@gmail.com>
2025-01-12 14:43:29 -0500

    fix: disable edge runtime by default to enable windows development (#1281)
    * fix: disable edge runtime by default to enable windows development
    
    * Discard changes to README.md
    
    * Apply suggestions from code review
    
    * Update apps/nextjs/src/app/api/auth/[...nextauth]/route.ts
    
    * fmt
    
    ---------
    
    Co-authored-by: Julius Marminge <jmarminge@gmail.com>
    Co-authored-by: juliusmarminge <julius0216@outlook.com>

--- apps/nextjs/src/app/api/trpc/[trpc]/route.ts
+++ apps/nextjs/src/app/api/trpc/[trpc]/route.ts
@@ -3,8 +3,6 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter, createTRPCContext } from "@acme/api";
 import { auth } from "@acme/auth";
 
-export const runtime = "edge";
-
 /**
  * Configure basic CORS headers
  * You should extend this to match your needs

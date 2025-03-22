73270b867cae9a3cb9ed78b4c14b5f684d4aff17
Julius Marminge <julius0216@outlook.com>
2025-03-22 22:15:59 +0100

    trpc11 stable (#1319)
    * trpc11 stable
    
    * vercel man...
    
    * no more unstable link
    
    * kewl

--- apps/nextjs/src/trpc/react.tsx
+++ apps/nextjs/src/trpc/react.tsx
@@ -5,8 +5,8 @@ import { useState } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import {
   createTRPCClient,
+  httpBatchStreamLink,
   loggerLink,
-  unstable_httpBatchStreamLink,
 } from "@trpc/client";
 import { createTRPCContext } from "@trpc/tanstack-react-query";
 import SuperJSON from "superjson";
@@ -40,7 +40,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
             env.NODE_ENV === "development" ||
             (op.direction === "down" && op.result instanceof Error),
         }),
-        unstable_httpBatchStreamLink({
+        httpBatchStreamLink({
           transformer: SuperJSON,
           url: getBaseUrl() + "/api/trpc",
           headers() {

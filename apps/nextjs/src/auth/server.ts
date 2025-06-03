ed8acc43906623e9ccd4c365d45e31b379ded6d9
juliusmarminge <jmarminge@gmail.com>
2025-06-03 16:45:19 +0200

    better

--- apps/nextjs/src/auth/server.ts
+++ apps/nextjs/src/auth/server.ts
@@ -7,11 +7,12 @@ import { initAuth } from "@acme/auth";
 
 import { env } from "~/env";
 
-const baseUrl = env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : env.VERCEL_URL
-    ? `https://${env.VERCEL_URL}`
-    : "http://localhost:3000";
+const baseUrl =
+  env.VERCEL_ENV === "production"
+    ? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : env.VERCEL_ENV === "preview"
+      ? `https://${env.VERCEL_URL}`
+      : "http://localhost:3000";
 
 export const auth = initAuth({
   baseUrl,

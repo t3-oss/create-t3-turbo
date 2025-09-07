5604f92672de2732d7638d5ba2a04255ea644412
Carlos Ricardo Ziegler <38855507+CarlosZiegler@users.noreply.github.com>
2025-09-07 18:36:04 +0200

    Fix Better-Auth CLI Schema Generation Issue (#1377)
    * feat: fix better-auth cli
    
    * ignore lint
    
    ---------
    
    Co-authored-by: Julius Marminge <julius0216@outlook.com>

--- packages/auth/eslint.config.js
+++ packages/auth/eslint.config.js
@@ -3,7 +3,7 @@ import baseConfig, { restrictEnvAccess } from "@acme/eslint-config/base";
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: [],
+    ignores: ["script/**"],
   },
   ...baseConfig,
   ...restrictEnvAccess,

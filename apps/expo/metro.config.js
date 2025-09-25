9e7706a1c3c30ed326d8f867a4bba871b241815d
Vanja Popovic <vaniapopovic@icloud.com>
2025-09-25 19:04:14 +0100

    chore: simplify metro config

--- apps/expo/metro.config.js
+++ apps/expo/metro.config.js
@@ -2,25 +2,16 @@
 const path = require("node:path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { FileStore } = require("metro-cache");
-const { withReactNativeCSS } = require("react-native-css/metro");
 const { withNativewind } = require("nativewind/metro");
 
+const config = getDefaultConfig(__dirname);
+
+config.cacheStores = [
+  new FileStore({ root: path.join(__dirname, "node_modules", ".cache", "metro") }),
+];
+
 /** @type {import('expo/metro-config').MetroConfig} */
-module.exports = withTurborepoManagedCache(
-  withNativewind(getDefaultConfig(__dirname)),
+module.exports = withNativewind(
+  config,
 );
 
-/**
- * Move the Metro cache to the `.cache/metro` folder.
- * If you have any environment variables, you can configure Turborepo to invalidate it when needed.
- *
- * @see https://turborepo.com/docs/reference/configuration#env
- * @param {import('expo/metro-config').MetroConfig} config
- * @returns {import('expo/metro-config').MetroConfig}
- */
-function withTurborepoManagedCache(config) {
-  config.cacheStores = [
-    new FileStore({ root: path.join(__dirname, ".cache/metro") }),
-  ];
-  return config;
-}

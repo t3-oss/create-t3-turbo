e6e5e2ed394de7d4e332ab1b505d8e214d73fa2c
Julius Marminge <julius0216@outlook.com>
2025-10-24 20:09:55 -0700

    use next 16 (#1447)

--- packages/ui/src/separator.tsx
+++ packages/ui/src/separator.tsx
@@ -16,7 +16,7 @@ export function Separator({
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px",
+        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
         className,
       )}
       {...props}

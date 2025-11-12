7299a3084fc2490d6edab9c96767f2a5e628aa91
Julius <julius0216@outlook.com>
2025-11-12 10:56:33 -0800

    use locallink to skip http when we're already on our server

--- apps/tanstack-start/src/lib/trpc.ts
+++ apps/tanstack-start/src/lib/trpc.ts
@@ -1,33 +1,55 @@
+import { createIsomorphicFn } from "@tanstack/react-start";
+import { getRequestHeaders } from "@tanstack/react-start/server";
 import {
   createTRPCClient,
   httpBatchStreamLink,
   loggerLink,
+  unstable_localLink,
 } from "@trpc/client";
 import { createTRPCContext } from "@trpc/tanstack-react-query";
 import SuperJSON from "superjson";
 
-import type { AppRouter } from "@acme/api";
+import * as Api from "@acme/api";
 
+import { auth } from "~/auth/server";
 import { env } from "~/env";
 import { getBaseUrl } from "~/lib/url";
 
-export const trpcClient = createTRPCClient<AppRouter>({
-  links: [
-    loggerLink({
-      enabled: (op) =>
-        env.NODE_ENV === "development" ||
-        (op.direction === "down" && op.result instanceof Error),
-    }),
-    httpBatchStreamLink({
-      transformer: SuperJSON,
-      url: getBaseUrl() + "/api/trpc",
-      headers() {
-        const headers = new Headers();
-        headers.set("x-trpc-source", "nextjs-react");
-        return headers;
-      },
-    }),
-  ],
-});
+export const makeTRPCClient = createIsomorphicFn()
+  .server(() => {
+    return createTRPCClient<Api.AppRouter>({
+      links: [
+        unstable_localLink({
+          router: Api.appRouter,
+          transformer: SuperJSON,
+          createContext: () =>
+            Api.createTRPCContext({
+              auth: auth,
+              headers: getRequestHeaders(),
+            }),
+        }),
+      ],
+    });
+  })
+  .client(() => {
+    return createTRPCClient<Api.AppRouter>({
+      links: [
+        loggerLink({
+          enabled: (op) =>
+            env.NODE_ENV === "development" ||
+            (op.direction === "down" && op.result instanceof Error),
+        }),
+        httpBatchStreamLink({
+          transformer: SuperJSON,
+          url: getBaseUrl() + "/api/trpc",
+          headers() {
+            const headers = new Headers();
+            headers.set("x-trpc-source", "nextjs-react");
+            return headers;
+          },
+        }),
+      ],
+    });
+  });
 
-export const { useTRPC, TRPCProvider } = createTRPCContext<AppRouter>();
+export const { useTRPC, TRPCProvider } = createTRPCContext<Api.AppRouter>();
